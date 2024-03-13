12b7b37687577bbcacd800407bfac836a29c70d4
Alexandra Schukina <alexandra.kovalenko1999@mail.ru>
2024-03-13 12:09:31 +0300

    edited tests, add new test

--- src/test/java/ru/netology/card/CardTest.java
+++ src/test/java/ru/netology/card/CardTest.java
@@ -14,6 +14,7 @@ import org.openqa.selenium.chrome.ChromeOptions;
 import java.util.List;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.openqa.selenium.By.className;
 
 public class CardTest {
     private WebDriver driver;
@@ -45,21 +46,21 @@ public class CardTest {
         List<WebElement> elements = driver.findElements(By.className("input__control"));
         elements.get(0).sendKeys("Петров Петя");
         elements.get(1).sendKeys("+79102671142");
-        driver.findElements(By.className("checkbox__box")).click();
-        driver.findElements(By.className("button")).click();
+        driver.findElement(By.className("checkbox__box")).click();
+        driver.findElement(By.className("button")).click();
 
-        String text = driver.findElement(By.className("order-success")).getText();
+        String text = driver.findElement(By.cssSelector("[data-test-id=order-success]")).getText();
         assertEquals("Ваша заявка успешно отправлена! Наш менеджер свяжется с вами в ближайшее время.", text.trim());
     }
 
     // тест с именем на латинице
     @Test
     void invalidNameTest() {
-        List<WebElement> elements = driver.findElements(By.className("input__control"));
+        List<WebElement> elements = driver.findElements(className("input__control"));
         elements.get(0).sendKeys("Petrov Petya");
         elements.get(1).sendKeys("+79102671142");
-        driver.findElements(By.className("checkbox__box")).click();
-        driver.findElements(By.className("button")).click();
+        driver.findElement(className("checkbox__box")).click();
+        driver.findElement(className("button")).click();
 
         String expected = "Имя и Фамилия указаные неверно. Допустимы только русские буквы, пробелы и дефисы.";
         String actual = driver.findElement(By.cssSelector("[data-test-id=name].input_invalid .input__sub")).getText().trim();
@@ -70,10 +71,10 @@ public class CardTest {
     // тест с неверным номером телефона
     @Test
     void invalidPhoneTest() {
-        List<WebElement> elements = driver.findElements(By.className("input__control"));
+        List<WebElement> elements = driver.findElements(className("input__control"));
         elements.get(0).sendKeys("Петров Петя");
         elements.get(1).sendKeys("+00909");
-        driver.findElements(By.className("button")).click();
+        driver.findElement(className("button")).click();
 
         String expected = "Телефон указан неверно. Должно быть 11 цифр, например, +79012345678.";
         String actual = driver.findElement(By.cssSelector("[data-test-id=phone].input_invalid .input__sub")).getText().trim();
@@ -84,13 +85,13 @@ public class CardTest {
     // пустое имя
     @Test
     void nameNullTest() {
-        List<WebElement> elements = driver.findElements(By.className("input__control"));
+        List<WebElement> elements = driver.findElements(className("input__control"));
         elements.get(1).sendKeys("+79102671142");
-        driver.findElements(By.className("checkbox__box")).click();
-        driver.findElements(By.className("button")).click();
+        driver.findElement(className("checkbox__box")).click();
+        driver.findElement(className("button")).click();
 
         String expected = "Поле обязательно для заполнения";
-        String actual = driver.findElement(By.cssSelector("[data-test-id=phone].input_invalid .input__sub")).getText().trim();
+        String actual = driver.findElement(By.cssSelector("[data-test-id=name].input_invalid .input__sub")).getText().trim();
 
         assertEquals(expected, actual);
     }
@@ -98,10 +99,10 @@ public class CardTest {
     // пустой номер телефона
     @Test
     void phoneNullTest() {
-        List<WebElement> elements = driver.findElements(By.className("input__control"));
+        List<WebElement> elements = driver.findElements(className("input__control"));
         elements.get(0).sendKeys("Петров Петя");
-        driver.findElements(By.className("checkbox__box")).click();
-        driver.findElements(By.className("button")).click();
+        driver.findElement(className("checkbox__box")).click();
+        driver.findElement(className("button")).click();
 
         String expected = "Поле обязательно для заполнения";
         String actual = driver.findElement(By.cssSelector("[data-test-id=phone].input_invalid .input__sub")).getText().trim();
@@ -112,14 +113,27 @@ public class CardTest {
     // пропущен чек-бокс
     @Test
     void checkBoxNullTest() {
-        List<WebElement> elements = driver.findElements(By.className("input__control"));
+        List<WebElement> elements = driver.findElements(className("input__control"));
         elements.get(0).sendKeys("Петров Петя");
         elements.get(1).sendKeys("+79102671142");
-        driver.findElements(By.className("button")).click();
+        driver.findElement(className("button")).click();
 
         String expected = "Я соглашаюсь с условиями обработки и использования моих персональных данных и разрешаю сделать запрос в бюро кредитных историй";
         String actual = driver.findElement(By.cssSelector("[data-test-id=agreement].input_invalid .checkbox__text")).getText().trim();
 
         assertEquals(expected, actual);
+    }
+
+    // тест с фамилией через дефиз
+    @Test
+    void validTestLastName() {
+        List<WebElement> elements = driver.findElements(By.className("input__control"));
+        elements.get(0).sendKeys("Петров-Иванов");
+        elements.get(1).sendKeys("+79102671142");
+        driver.findElement(By.className("checkbox__box")).click();
+        driver.findElement(By.className("button")).click();
+
+        String text = driver.findElement(By.cssSelector("[data-test-id=order-success]")).getText();
+        assertEquals("Ваша заявка успешно отправлена! Наш менеджер свяжется с вами в ближайшее время.", text.trim());
     }
 }
